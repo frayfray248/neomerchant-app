0ad74290d99090f583c5d069b180045a45abe79e
frayfray248 <nicman248@hotmail.com>
2023-02-04 20:48:42 -0800

    added Formik to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Formik } from 'formik';
 import { useState } from 'react';
 
 function LoginForm({ handleLogin }) {
@@ -9,39 +10,79 @@ function LoginForm({ handleLogin }) {
     const [errorMessage, setErrorMessage] = useState('')
 
 
-    const handleLoginSubmit = (e) => {
-        e.preventDefault();
-        handleLogin(username, password)
-        .catch(message => {
-            setErrorMessage(message)
-        }
-            )
+    const handleLoginSubmit = (values, { setSubmitting }) => {
+
+        handleLogin(values.username, values.password)
+            .then(() => {
+                setSubmitting(false)
+            })
+            .catch(message => {
+                setErrorMessage(message)
+            })
+    }
+
+    const validate = values => {
+
     }
 
     return (
-        <Form onSubmit={handleLoginSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter email"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </Form.Group>
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            validate={validate}
+            onSubmit={handleLoginSubmit}
+        >
+            {({
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </Form.Group>
-            <p>{errorMessage}</p>
-            <Button variant="primary" type="submit">
-                Login
-            </Button>
-        </Form>
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+
+            }) => (
+
+                
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="loginFormUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.username}
+
+                        />
+                        {errors.username && touched.username && errors.username}
+                    </Form.Group>
+
+
+                    <Form.Group className="mb-3" controlId="loginFormPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+
+                        />
+                        {errors.password && touched.password && errors.password}
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" disabled={isSubmitting}>
+                        Login
+                    </Button>
+                    {errorMessage}
+                </Form>
+            )}
+        </Formik>
     )
 }
 
